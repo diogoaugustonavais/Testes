570c54f24434a9f934c4f29f6154407b0351dff0
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-17 16:12:36 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,35 +1,33 @@
 let nome = prompt("Qual é o seu nome?");
 alert("Olá "+ nome + ", seja bem vindo ao jogo!");
 let arma = prompt("Qual arma você deseja usar? (pedra(1), espada(2), arco e flecha(3))");
-if (arma == "pedra"+"1"){
+if (arma == "pedra" || arma == "1"){
     alert("Você escolheu a pedra!")
-}else if (arma == "espada"+"2"){
+}else if (arma == "espada" || arma == "2"){
     alert("Você escolheu a espada!")
-}else if (arma == "arco e flecha"+"3"){
+}else if (arma == "arco e flecha" || arma == "3"){
     alert("Você escolheu o arco e flecha!")
 }
 alert("Você está caminhando pela floresta e encontra um inimigo! O que você faz?");
 let acao = prompt("Digite o número da ação que deseja realizar: (atacar(1), fugir(2))");
-if (acao == "atacar"+"1"){
+if (acao == "atacar" || acao == "1"){
     alert("Você decide atacar o inimigo!")
-    if (arma == "pedra"+"1"){
-        while (vidaInimigo != 0){
-        let ataque = prompt("Qual tipo de ataque deseja usar? (pedrada(1))")
-        if (ataque == "pedrada"+"1"){
-            const inimigo = 100
+    if (arma == "pedra" || arma == "1"){
+        let vidaInimigo = 100
+            while (vidaInimigo > 0){
+                let ataque = prompt("Qual tipo de ataque deseja usar? (pedrada(1))")
+        if (ataque == "pedrada" || ataque == "1"){
             const dano = danopedra(ataque)
-            const vidaInimigo = vida(inimigo, dano)
-            console.log("O inimigo tem ${vidaInimigo} de vida restante.");
+            vidaInimigo = vidaInimigo - dano
+            console.log("O inimigo tem " + vidaInimigo + " de vida restante.")
+            alert("O inimigo tem " + vidaInimigo + " de vida restante.")
+            }
     }  
-    }  
-}
-    
+     
+ }
 }
 function danopedra(ataque){
-    if(ataque == "pedrada"){
+    if(ataque == "pedrada" || ataque == "1"){
         return 10
     }
-}
-function vida(inimigo, dano){
-    return inimigo - dano
-}
+}   
