04c7f8c3ca5fa07c9ad2b4c075caa072f8483535
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-18 19:16:41 -0300

    Implement combat mechanics for player and enemy

--- jogo.js
+++ jogo.js
@@ -12,7 +12,97 @@ alert("Você está caminhando pela floresta e encontra um inimigo! O que você f
 let acao = prompt("Digite o número da ação que deseja realizar: (atacar(1), fugir(2))");
 if (acao == "atacar" || acao == "1"){
     alert("Você decide atacar o inimigo!")
+  let vidaInimigo = 100
+  let vidaAtualPlayer = 100
+  let turno = 1 
+  while (vidaInimigo>0 && vidaAtualPlayer>0){
+    if (turno === 1){
     if (arma == "pedra" || arma == "1"){
+    vidaInimigo = ataquePedrada(vidaInimigo)
+    } else if (arma == "espada" || arma == "2"){
+    vidaInimigo = ataqueEspada(vidaInimigo)
+    } else if (arma == "arco e flecha" || arma == "3"){
+    vidaInimigo = ataqueArco(vidaInimigo)
+    } turno = 2
+  }
+   else { vidaAtualPlayer = ataquedoinimigo(vidaAtualPlayer)
+    turno = 1
+  }
+}       
+} else if (acao == "fugir" || acao == "2"){
+   querocorrer()
+}
+function ataquePedrada(vidaAtualInimigo){
+     let ataque = prompt("Qual tipo de ataque deseja usar? (pedrada(1))")
+        if (ataque == "pedrada" || ataque == "1"){
+                const chancecritica = 0.4
+            if (Math.random() < chancecritica){
+        console.log("Você acertou um ataque critico!")
+        alert("Você acertou um ataque critico!")
+        const dano = 9
+        vidaAtualInimigo = vidaAtualInimigo - dano
+        console.log("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+        alert("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+    } else{
+            const dano = 3
+            vidaAtualInimigo = vidaAtualInimigo - dano
+            console.log("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+            alert("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+            }
+            return vidaAtualInimigo
+        }
+}
+function ataqueEspada(vidaAtualInimigo){
+     let ataque = prompt("Qual tipo de ataque deseja usar? (espadada(1))")
+        if (ataque == "espadada" || ataque == "1"){
+            const chancecritica = 0.2
+            if (Math.random() < chancecritica){
+        console.log("Você acertou um ataque critico!")
+        alert("Você acertou um ataque critico!")
+        const dano = 12
+        vidaAtualInimigo = vidaAtualInimigo - dano
+        console.log("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+        alert("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+    } else{
+            const dano = 7
+            vidaAtualInimigo = vidaAtualInimigo - dano
+            console.log("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+            alert("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+            }
+        }
+            return vidaAtualInimigo
+}
+function ataqueArco(vidaAtualInimigo){
+     let ataque = prompt("Qual tipo de ataque deseja usar? (flechada(1))")
+        if (ataque == "flechada" || ataque == "1"){
+            const chancecritica = 0.2
+            if (Math.random() < chancecritica){
+        console.log("Você acertou um ataque critico!")
+        alert("Você acertou um ataque critico!")
+        const dano = 11
+        vidaAtualInimigo = vidaAtualInimigo - dano
+        console.log("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+        alert("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+    } else{
+            const dano = 6
+            vidaAtualInimigo = vidaAtualInimigo - dano
+            console.log("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+            alert("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
+            }
+            return vidaAtualInimigo
+        }
+}
+function querocorrer(){
+    const chancefugir = 0.1
+    if (Math.random() < chancefugir){
+    console.log("Você conseguiu fugir!!!")
+    alert("Você conseguiu fugir!!!")
+    } else{
+    alert("Você decide fugir do inimigo! Você NÃO conseguiu escapar!"+"\n Perdeu aura.")
+    console.log("Você decide fugir do inimigo! Você NÃO conseguiu escapar!"+"\n Perdeu aura.")
+    alert("Você foi obrigado a atacar o inimigo!")
+    console.log("Você foi obrigado a atacar o inimigo!")
+     if (arma == "pedra" || arma == "1"){
         let vidaInimigo = 100
             while (vidaInimigo > 0){
                vidaInimigo = ataquePedrada(vidaInimigo)
@@ -27,14 +117,19 @@ if (acao == "atacar" || acao == "1"){
             while (vidaInimigo > 0){
                vidaInimigo = ataqueArco(vidaInimigo)
             }
+        }
 }
-} else if (acao == "fugir" || acao == "2"){
-   querocorrer()
 }
-function ataquePedrada(vidaAtualInimigo){
-     let ataque = prompt("Qual tipo de ataque deseja usar? (pedrada(1))")
-        if (ataque == "pedrada" || ataque == "1"){
-                const chancecritica = 0.4
+function ataquedoinimigo(vidaAtualPlayer){
+  console.log("O inimigo te da um ataque!")
+  alert("O inimigo te da um ataque!")
+  let dano = 5
+   vidaAtualPlayer = vidaAtualPlayer - dano
+  console.log("O inimigo te deu "+dano+" de dano!\nVocê tem "+vidaAtualPlayer+" de vida.")
+  alert("O inimigo te deu "+dano+" de dano!\nVocê tem "+vidaAtualPlayer+" de vida.")
+  return vidaAtualPlayer
+  
+}                const chancecritica = 0.4
             if (Math.random() < chancecritica){
         console.log("Você acertou um ataque critico!")
         alert("Você acertou um ataque critico!")
