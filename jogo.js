f115373ea98f54eacfb493a4206a350f710b2f9f
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-18 22:28:42 -0300

    Refactor combat logic to include player turns

--- jogo.js
+++ jogo.js
@@ -102,21 +102,22 @@ function querocorrer(){
     console.log("Você decide fugir do inimigo! Você NÃO conseguiu escapar!"+"\n Perdeu aura.")
     alert("Você foi obrigado a atacar o inimigo!")
     console.log("Você foi obrigado a atacar o inimigo!")
-     if (arma == "pedra" || arma == "1"){
-        let vidaInimigo = 100
-            while (vidaInimigo > 0){
-               vidaInimigo = ataquePedrada(vidaInimigo)
-    }  
- } else if (arma == "espada" || arma == "2"){
     let vidaInimigo = 100
-            while (vidaInimigo > 0){
-               vidaInimigo = ataqueEspada(vidaInimigo)
- }
-}else if (arma == "arco e flecha" || arma == "3"){
-    let vidaInimigo = 100
-            while (vidaInimigo > 0){
-               vidaInimigo = ataqueArco(vidaInimigo)
-            }
+    let vidaAtualPlayer = 100
+    let turno = 1 
+    while (vidaInimigo>0 && vidaAtualPlayer>0){
+    if (turno === 1){
+    if (arma == "pedra" || arma == "1"){
+    vidaInimigo = ataquePedrada(vidaInimigo)
+    } else if (arma == "espada" || arma == "2"){
+    vidaInimigo = ataqueEspada(vidaInimigo)
+    } else if (arma == "arco e flecha" || arma == "3"){
+    vidaInimigo = ataqueArco(vidaInimigo)
+    } turno = 2
+  }
+   else { vidaAtualPlayer = ataquedoinimigo(vidaAtualPlayer)
+    turno = 1
+        }
         }
 }
 }
@@ -128,21 +129,4 @@ function ataquedoinimigo(vidaAtualPlayer){
   console.log("O inimigo te deu "+dano+" de dano!\nVocê tem "+vidaAtualPlayer+" de vida.")
   alert("O inimigo te deu "+dano+" de dano!\nVocê tem "+vidaAtualPlayer+" de vida.")
   return vidaAtualPlayer
-  
-}                const chancecritica = 0.4
-            if (Math.random() < chancecritica){
-        console.log("Você acertou um ataque critico!")
-        alert("Você acertou um ataque critico!")
-        const dano = 9
-        vidaAtualInimigo = vidaAtualInimigo - dano
-        console.log("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
-        alert("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
-    } else{
-            const dano = 3
-            vidaAtualInimigo = vidaAtualInimigo - dano
-            console.log("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
-            alert("O inimigo tem " + vidaAtualInimigo + " de vida restante.")
-            }
-            return vidaAtualInimigo
-        }
 }
